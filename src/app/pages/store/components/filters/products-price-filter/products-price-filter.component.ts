38d938106078227f38d37ffeeb0e2f60cb5d4910
Vladyslav Lintur <vladyslavlintur@gmail.com>
2022-11-10 18:02:10 +0300

    Store page: update products-price-filter

--- src/app/pages/store/components/filters/products-price-filter/products-price-filter.component.ts
+++ src/app/pages/store/components/filters/products-price-filter/products-price-filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-products-price-filter',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-price-filter.component.css']
 })
 export class ProductsPriceFilterComponent implements OnInit {
+  private readonly minValue = 0;
+  private readonly maxValue = 100000;
+  private destroyed$: Subject<void> = new Subject();
+  
 
-  constructor() { }
+  readonly priceFilterForm = new FormGroup({
+    minValue: new FormControl<number>(this.minValue, Validators.min(this.minValue)),
+    maxValue: new FormControl<number>(this.maxValue, Validators.max(this.maxValue)),
+  });
 
   ngOnInit(): void {
+    this.priceFilterForm.valueChanges.pipe(takeUntil(this.destroyed$)).subscribe(() => {
+      const { minValue, maxValue } = this.priceFilterForm.controls;
+
+      switch(true){
+        case maxValue.invalid:
+          maxValue.setValue(this.maxValue);
+          break;
+        case minValue.invalid:
+          minValue.setValue(this.minValue);
+          break;
+        case minValue.value! > maxValue.value!:
+          minValue.setValue(maxValue.value);
+          break;
+        case maxValue.value! < minValue.value!:
+          maxValue.setValue(minValue.value);
+          break;
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 }
