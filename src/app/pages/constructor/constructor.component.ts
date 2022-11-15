1b10959529fd7ce3f40ee2718f89931b183c6c26
alex-kostuyk <morsskott@gmail.com>
2022-11-15 15:54:05 +0200

    Constructor Page: Add subpage navigation and product list

--- src/app/pages/constructor/constructor.component.ts
+++ src/app/pages/constructor/constructor.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreItem } from 'src/app/models/StoreItem.model';
+import { StoreService } from 'src/app/services/store.service';
+import { MatDrawerMode } from '@angular/material/sidenav';
+import { MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-constructor',
   templateUrl: './constructor.component.html',
   styleUrls: ['./constructor.component.css']
 })
-export class ConstructorComponent implements OnInit {
 
-  constructor() { }
+
+export class ConstructorComponent implements OnInit {
+  mediaSubscription: Subscription | undefined;
+  productsSubscription: Subscription | undefined;
+  activeMediaQuery = '';
+  products: Array<StoreItem> = [];
+  mode: MatDrawerMode = "side";
+  opened: boolean = true;
+
+  constructor(
+    private mediaObserver: MediaObserver,
+    private storeServise: StoreService
+  ) { }
 
   ngOnInit(): void {
+    this.getMockProducts();
+
+    this.mediaSubscription = this.mediaObserver.asObservable().subscribe((change) => {
+      change.forEach((item) => {
+        this.activeMediaQuery = item ? `'${item.mqAlias}' = (${item.mediaQuery})` : '';
+        if (item.mqAlias == 'xs' || item.mqAlias == 'sm') {
+          this.closeDrawer();
+        }
+        else if (item.mqAlias == 'gt-sm') {
+          this.openDrawer();
+        }
+      });
+    });
   }
 
+  openDrawer() {
+    this.opened = true;
+  }
+
+  closeDrawer() {
+    this.opened = false;
+  }
+
+  getProducts(): void {
+    this.productsSubscription = this.storeServise.getAllProducts().subscribe((data) => {
+      data.forEach((item) => {
+        item.imageUrl = `https://localhost:7006/images/${item.imageUrl}`;
+      });
+
+      this.products = data;
+    });
+  }
+
+  getMockProducts() {
+    this.products = this.storeServise.getMockProducts();
+  }
+
+  ngOnDestroy(): void {
+    this.mediaSubscription?.unsubscribe();
+  }
 }
